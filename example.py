cc7a4899e6bced6d56a15d391a724e1e4ee1b47f
keh9mark <keh9mark@yandex.ru>
2021-12-13 20:52:40 +0300

    + category request

--- example.py
+++ example.py
@@ -6,5 +6,5 @@ if __name__ == '__main__':
     catalog = IMGCatalog()
     response = requests.get('http://localhost:8080/?')
     print(response.content)
-    response = requests.get('http://localhost:8080/?')
+    response = requests.get('http://localhost:8080/?category[]=category_6&category[]=category_5')
     print(response.content)
