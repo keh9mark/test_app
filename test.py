6793332bcdde834d8b64c4c64da280892df573af
Nikolay Markov <keh9mark@yandex.ru>
2022-05-26 13:50:18 +0300

    рабочая версия отрисовки графика

--- test.py
+++ test.py
@@ -1,26 +1,143 @@
-from mpl_toolkits.axes_grid1 import host_subplot
-from mpl_toolkits import axisartist
 import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
 
 
-dog = [10, 20, 15, 10]
-activity = [5, 30, 150, 5]
+def make_graph_image(items, merge_info):
+  x_ = []
+  y_ = []
+  points_x = []
+  points_y = []
 
-fig, ax = plt.subplots()
-ax.plot(activity, dog)
-ax.grid(True)
+  sorted_merge = []
 
-ax.scatter([5, 30, 150], [10, 20, 15])
+  for merge in merge_info:
+    first = merge[0]
+    second = merge[1]
+    sorted_merge.append(first)
+    x_.append(items[first]["x"])
+    y_.append(items[first]["y"])
+    points_x.append(items[first]["x"])
+    points_y.append(items[first]["y"])
 
-ax.text(
-  5,
-  10,
-  "А \nS1=10,5+j30 \nI=30%, II=40%, III=30% \nUном=10, Кз=1.6",
-  {},
-  rotation=0,
-)
+  x_.append(items[second]["x"])
+  y_.append(items[second]["y"])
 
-ax.legend()
+  fig = plt.figure()
+  gs = gridspec.GridSpec(1, 3)
+  ax = fig.add_subplot(gs[0, :2])
+
+  ax.plot(x_, y_, color="k")
+
+  ax.grid(True)
+
+  ax.scatter(points_x, points_y, color="k")
+
+  for x, y, point in zip(points_x, points_y, sorted_merge):
+    ax.text(
+      x - 10,
+      y + 2.5,
+      f"{point}({x},{y})",
+      {},
+      rotation=0,
+      bbox=dict(
+        boxstyle="square",
+        ec=(0, 0, 0),
+        fc=(1, 1, 1),
+      ),
+    )
+
+  x_coord = 0
+  y_coord = 1
+  if len(items) == 6:
+    y_coord = 0.9
+
+  ax = fig.add_subplot(gs[0, 2])
+
+  for point, info in items.items():
+    ax.text(
+      x_coord,
+      y_coord,
+      f"({point}) {info['S']} \n{info['proc']} \nU={info['Unom']}, кз=1.6",
+      {},
+      rotation=0,
+      bbox=dict(
+        boxstyle="square",
+        ec=(0, 0, 0),
+        fc=(1, 1, 1),
+      ),
+    )
+    y_coord -= 0.18
+
+  ax.set_axis_off()
+
+  plt.xticks([])
+  plt.yticks([])
+
+  image_name = "myimage.png"
+  plt.savefig(image_name)
+  return image_name
 
 
-plt.show()
+if __name__ == "__main__":
+  items = {
+    "A": {
+      "x": 10,
+      "y": 40,
+      "S": "S=10+j40",
+      "coef": "Кз=1,3",
+      "proc": "I=30%, II=40%, III=30%",
+      "Unom": 6,
+    },
+    "B": {
+      "x": 70,
+      "y": 20,
+      "S": "S=10+j40",
+      "coef": "Кз=1,3",
+      "proc": "I=0%, II=70%, III=30%",
+      "Unom": 6,
+    },
+    "1": {
+      "x": 30,
+      "y": 80,
+      "S": "S=10+j40",
+      "coef": "Кз=1,3",
+      "proc": "I=30%, II=40%, III=30%",
+      "Unom": 6,
+    },
+    "2": {
+      "x": 90,
+      "y": 60,
+      "S": "S=10+j40",
+      "coef": "Кз=1,3",
+      "proc": "I=30%, II=40%, III=30%",
+      "Unom": 6,
+    },
+    "3": {
+      "x": 40,
+      "y": 10,
+      "S": "S=10+j40",
+      "coef": "Кз=1,3",
+      "proc": "I=30%, II=40%, III=30%",
+      "Unom": 6,
+    },
+    "4": {
+      "x": 90,
+      "y": 10,
+      "S": "S=10+j40",
+      "coef": "Кз=1,3",
+      "proc": "I=30%, II=40%, III=30%",
+      "Unom": 6,
+    },
+    "5": {
+      "x": 40,
+      "y": 40,
+      "S": "S=10+j40",
+      "coef": "Кз=1,3",
+      "proc": "I=30%, II=40%, III=30%",
+      "Unom": 6,
+    },
+  }
+
+  merge_info = ["A1", "12", "2B", "B4", "43", "35", "5A"]
+
+  make_graph_image(items=items, merge_info=merge_info)
